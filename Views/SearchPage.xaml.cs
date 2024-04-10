6926f8a31d1348b541f422bd534091cd33e155a1
billonnet3 <alexandre.billonnet@etu.unilim.fr>
2024-04-10 23:05:04 +0200

    add step 1

---
+++ Views/SearchPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlexandreApp.Views;
+
+public partial class SearchPage : ContentPage
+{
+    public SearchPage()
+    {
+        InitializeComponent();
+    }
+}
--- obj/Debug/net8.0-ios/iossimulator-x64/AlexandreApp.AssemblyInfo.cs
+++ obj/Debug/net8.0-ios/iossimulator-x64/AlexandreApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AlexandreApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+8f2386dc82a73aa3a5a3f9ce7f61bd8dd4366a01")]
 [assembly: System.Reflection.AssemblyProductAttribute("AlexandreApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AlexandreApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
